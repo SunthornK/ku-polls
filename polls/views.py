c0aadfdf2273ff479e94c65dd1849c9aefaf55c1
Sunthorn KOMPITA <sunthorn.ko@ku.th>
2024-09-01 05:03:43 +0700

    Modify views code to use two new method

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
 from django.db.models import F
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-
+from django.contrib import messages
 from .models import Choice, Question
 
 
@@ -21,7 +21,22 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
+    object = Question
     template_name = "polls/detail.html"
+
+    def get_detail(self, request, *args, **kwargs):
+        try:
+            self.object = get_object_or_404(Question, pk=kwargs['pk'])
+        except Http404:
+            messages.error(request, f"Poll with ID {kwargs['pk']} is not exist")
+            return redirect("polls:index")
+        if not self.object.can_vote():
+            messages.error(request, f"Poll number {self.object.pk} has ended, which is not available for voting.")
+            return redirect("polls:index")
+        if not self.object.is_published():
+            messages.error(request, f"Poll number {self.object.pk} is not available")
+            return redirect("polls:index")
+        return render(request, self.template_name, {"question": self.object})
 
 
 class ResultsView(generic.DetailView):
@@ -34,7 +49,6 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(
             request,
             "polls/detail.html",
@@ -46,7 +60,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
